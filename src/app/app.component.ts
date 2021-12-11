d47c890641396703ad4d66595db5a781b0d97145
zakaria011 <maskanzakaria@gmail.com>
2021-12-11 15:49:46 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['../assets/vendors/feather/feather.css',
+                '../assets/vendors/mdi/css/materialdesignicons.min.css',
+                '../assets/vendors/ti-icons/css/themify-icons.css',
+                '../assets/vendors/typicons/typicons.css',
+                '../assets/vendors/simple-line-icons/css/simple-line-icons.css',
+               '../assets/vendors/css/vendor.bundle.base.css',
+               '../assets/css/vertical-layout-light/style.css'
+               //'../assets/vendors/datatables.net-bs4/dataTables.bootstrap4.css',
+              //'../assets/js/select.dataTables.min.css'
+
+]
 })
 export class AppComponent {
   title = 'demande-soutien-a-dashboard';
